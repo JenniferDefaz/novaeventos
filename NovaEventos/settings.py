85da57118ebcc7aee9b7ee9207178cd67e1a7923
Jennifer Defaz <jenniferdefaz991@gmail.com>
2026-07-29 23:50:12 -0500

    Corrige CSRF_TRUSTED_ORIGINS y SSL proxy header para Railway

--- NovaEventos/settings.py
+++ NovaEventos/settings.py
@@ -20,6 +20,14 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1').split(',')
 
+CSRF_TRUSTED_ORIGINS = config('CSRF_TRUSTED_ORIGINS', default='').split(',') if config('CSRF_TRUSTED_ORIGINS', default='') else []
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+
+
+
+
 
 # Application definition
 
