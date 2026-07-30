b6f015405573290668b206b019ebfddce6a7806e
Jennifer Defaz <jenniferdefaz991@gmail.com>
2026-07-30 00:56:46 -0500

    Usar serve() para media files en produccion con DEBUG=False

--- NovaEventos/urls.py
+++ NovaEventos/urls.py
@@ -15,18 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-#Importando settings.py
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('eventos.urls'))
+    path('', include('eventos.urls')),
+    # Servir archivos media siempre, incluso con DEBUG=False
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
-
-# Servir archivos media siempre (local y producción)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
 
